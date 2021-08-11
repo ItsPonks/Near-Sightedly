4dc536a17e9c79f3fe0ea5698a7524f714dc4d35
ItsPonks <jbras3@gmail.com>
2021-08-11 03:31:01 -0400

    Rewrite Mixins to be more reliable and pull values from config

--- src/main/java/its/ponks/nearsightedly/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/its/ponks/nearsightedly/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,47 +1,63 @@
 package its.ponks.nearsightedly.mixin;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import its.ponks.nearsightedly.NSConfig;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.option.Option;
-import net.minecraft.network.packet.s2c.play.ChunkLoadDistanceS2CPacket;
-import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
 import net.minecraft.util.math.MathHelper;
 
-@SuppressWarnings("unused")
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
-	private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
+	@Shadow
+	private int chunkLoadDistance;
+	@Shadow
+	@Final
+	private MinecraftClient client;
 	private double previous;
 
-	@Inject(at = @At("HEAD"), method = "clearWorld")
-	private void clearWorld(CallbackInfo info) {
-		if (!CLIENT.isInSingleplayer()) {
-			System.out.println("Resetting client render distance to " + previous + "...");
-			Option.RENDER_DISTANCE.set(CLIENT.options, previous);
+	@Inject(at = @At("TAIL"), method = { "clearWorld" })
+	private void clearWorld(@SuppressWarnings("unused") CallbackInfo info) {
+		if (!client.isInSingleplayer()) {
+			var current = Option.RENDER_DISTANCE.get(client.options);
+			if (current != previous) {
+				System.out.println("Resetting render distance from " + current + " to " + previous + "...");
+				Option.RENDER_DISTANCE.set(client.options, previous);
+			}
+
+			previous = 0;
 		}
 	}
 
-	@Inject(at = @At("HEAD"), method = "onChunkLoadDistance")
-	private void onChunkLoadDistance(ChunkLoadDistanceS2CPacket packet, CallbackInfo info) {
-		set(packet.getDistance());
-	}
+	@Inject(at = @At("TAIL"), method = { "onGameJoin", "onChunkLoadDistance" })
+	private void onGameJoinAndChunkLoadDistance(@SuppressWarnings("unused") CallbackInfo info) {
+		if (!client.isInSingleplayer()) {
+			var max = Option.RENDER_DISTANCE.getMax();
+			NSConfig.min = MathHelper.clamp(NSConfig.min, NSConfig.MIN, max);
+			NSConfig.max = MathHelper.clamp(NSConfig.max, NSConfig.MIN, max);
 
-	@Inject(at = @At("HEAD"), method = "onGameJoin")
-	private void onGameJoin(GameJoinS2CPacket packet, CallbackInfo info) {
-		set(packet.getViewDistance());
-	}
+			// For all those users who get min and max mixed up when configuring.
+			if (NSConfig.min > NSConfig.max) {
+				max = NSConfig.max;
+				NSConfig.max = NSConfig.min;
+				NSConfig.min = NSConfig.max;
+			}
 
-	private void set(double value) {
-		if (!CLIENT.isInSingleplayer()) {
-			previous = Option.RENDER_DISTANCE.get(CLIENT.options);
-			value = MathHelper.clamp(value, Option.RENDER_DISTANCE.getMin(), previous);
-			System.out.println("Setting client render distance to server view distance, clamped to " + value + "...");
-			Option.RENDER_DISTANCE.set(CLIENT.options, value);
+			var current = Option.RENDER_DISTANCE.get(client.options);
+			var value = MathHelper.clamp(chunkLoadDistance, NSConfig.min, NSConfig.max);
+			if (current != value) {
+				System.out.println("Changing render distance from " + current + " to " + value + "...");
+				Option.RENDER_DISTANCE.set(client.options, value);
+			}
+
+			if (previous == 0)
+				previous = current;
 		}
 	}
 }
